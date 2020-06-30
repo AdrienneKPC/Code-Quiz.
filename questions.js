d353f89b322869d7a9cbc3c42bcdc993902e906a
Adrienne Knight <adriennek1@gmail.com>
2020-06-30 15:10:35 -0600

    created JS file and wrote question variable

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        titleOfQuestion: "What are the types of CSS selectors?",
+        choice: ["<Element, ID, Class, Universal, Group>", "<ID, Class, Element>", "<javascript, querySelector, DOM>", "<What is CSS?>"],
+        answer: "<Element, ID, Class, Universal, Group>"
+        },
+    {
+        titleOfQuestion: "Where should you insert JavaScript in an HTML page?",
+        choice: ["the <head> tag", "in the <body>tag", "inside the <body> or <head> tag", "Am I outside?"],
+        answer: "inside the <body> or <head> tag"
+        },
+    {
+        titleOfQuestion: "Why would you use a for loop?",
+        choice: ["to give someone a heads up that you've hit into them on the fairway", "to repeat a segment of code", "to call a function", "to confuse yourself"],
+        answer: "to repeat a segment of code"
+        },
+    {
+        titleOfQuestion: "What is JQuery?",
+        choice: ["just JS in a simplified syntax", "indecipherable alien writing", "a JS library", "both a library and a syntax"],
+        answer: "both a library and a syntax"
+        },
+    {
+        titleOfQuestion: "Are cats or dogs better?",
+        choice: ["dogs", "dogs", "dogs", "dogs"],
+        answer: "dogs"
+        },
+]
